732494283ba0e68dbbed079a3656d60413917dcc
Long Vo <long_rmit@yahoo.com>
2022-11-30 18:55:42 -0500

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -9,17 +9,14 @@ class EmotionTable extends React.Component {
           <table className="table table-bordered">
             <tbody>
             {
-              Object.entries(this.props.emotions).map(function(mapentry) {
-                  return (
-                      <tr>
-                          <td>{mapentry[0]}</td>
-                          <td>{mapentry[1]}</td>
-                          <td>{mapentry[2]}</td>
-                          <td>{mapentry[3]}</td>
-                          <td>{mapentry[4]}</td>
-                      </tr>
-                  )
-              })
+             Object.entries(this.props.emotions).map(function(mapentry) {
+                return (
+                    <tr>
+                    <td>{mapentry[0]}</td>
+                    <td>{mapentry[1]}</td>
+                    </tr>
+                )
+                })
             }
             </tbody>
           </table>
